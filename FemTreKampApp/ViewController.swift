e9a8a17aa019f5436c137dedab57da9e1ae95541
Jani Pasanen <jani.m.pasanen@gmail.com>
2016-11-22 04:21:45 +0100

    Initial Commit

---
+++ FemTreKampApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FemTreKampApp
+//
+//  Created by Jani Pasanen on 2016-11-22.
+//  Copyright © 2016 Jani Pasanen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
