f160bd1ede5aea43034a1a7e4f228bd8a7e886ed
Jani Pasanen <jani.m.pasanen@gmail.com>
2016-11-26 21:57:07 +0100

    Modifieringar

---
+++ FemTreKampApp/JSONJoySerializationModel.swift
@@ -0,0 +1,14 @@
+//
+//  JSONJoySerializationModel.swift
+//  FemTreKampApp
+//
+//  Created by Jani Pasanen on 2016-11-26.
+//  Copyright © 2016 Jani Pasanen. All rights reserved.
+//
+
+import Foundation
+import JSONJoy
+
+class JSONJoySerializationModel {
+    
+}
---
+++ FemTreKampApp/SwiftHTTPModel.swift
@@ -0,0 +1,14 @@
+//
+//  SwiftHTTPModel.swift
+//  FemTreKampApp
+//
+//  Created by Jani Pasanen on 2016-11-26.
+//  Copyright © 2016 Jani Pasanen. All rights reserved.
+//
+
+import Foundation
+import SwiftHTTP
+
+class SwiftHTTPModel {
+    
+}
