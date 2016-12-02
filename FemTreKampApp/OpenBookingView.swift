11b0880c77904c6c3e56ea2dda1c2fe38d081623
Jani Pasanen <jani.m.pasanen@gmail.com>
2016-12-02 20:05:00 +0100

    ändringar av struktur

---
+++ FemTreKampApp/OpenBookingView.swift
@@ -0,0 +1,21 @@
+//
+//  OpenBookingView.swift
+//  FemTreKampApp
+//
+//  Created by Jani Pasanen on 2016-12-02.
+//  Copyright © 2016 Jani Pasanen. All rights reserved.
+//
+
+import UIKit
+
+class OpenBookingView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ FemTreKampApp/RegisterBookingCodeView.swift
@@ -0,0 +1,21 @@
+//
+//  RegisterBookingCodeView.swift
+//  FemTreKampApp
+//
+//  Created by Jani Pasanen on 2016-12-02.
+//  Copyright © 2016 Jani Pasanen. All rights reserved.
+//
+
+import UIKit
+
+class RegisterBookingCodeView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
