9984de82c31a79b4db90c9e50b538acf9b530b17
Jani Pasanen <jani.m.pasanen@gmail.com>
2016-12-02 20:15:03 +0100

    lade till classfiler

---
+++ FemTreKampApp/OpenBookingFromListView.swift
@@ -0,0 +1,21 @@
+//
+//  OpenBookingFromListView.swift
+//  FemTreKampApp
+//
+//  Created by Jani Pasanen on 2016-12-02.
+//  Copyright © 2016 Jani Pasanen. All rights reserved.
+//
+
+import UIKit
+
+class OpenBookingFromListView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ FemTreKampApp/RegisterBookingCompetitorsView.swift
@@ -0,0 +1,21 @@
+//
+//  RegisterBookingCompetitorsView.swift
+//  FemTreKampApp
+//
+//  Created by Jani Pasanen on 2016-12-02.
+//  Copyright © 2016 Jani Pasanen. All rights reserved.
+//
+
+import UIKit
+
+class RegisterBookingCompetitorsView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
